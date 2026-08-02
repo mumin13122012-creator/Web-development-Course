5fb7f3c24391b75f0bfead52e728ddb9b6736f28
mumin13122012-creator <mumin13122012@gmail.com>
2026-08-02 16:56:18 +0100

    Add files via upload

--- Lesson 28/script.js
+++ Lesson 28/script.js
@@ -1,77 +1,34 @@
-function getHistory() {
-    return document.getElementById("history-value").innerText;
+function calculate(){
+
+let bill = parseFloat(document.getElementById("billAmount").value);
+let tipPercentage = parseFloat(document.getElementById("tipPercentage").value);
+let people = parseInt(document.getElementById("numberOfPeople").value);
+
+if (isNaN(bill) || bill<0) {
+    alert("Please enter a valid bill amount.");
+    return;
 }
-function printHistory(num) {
-    document.getElementById("history-value").innerText = num;
+
+if (isNaN(tipPercentage) || tipPercentage<0) {
+    alert("Please enter a valid tip percentage.");
+    return;
 }
-function getOutput() {
-    return document.getElementById("output-value").innerText;
+
+let tipAmount= bill * (tipPercentage / 100);
+let totalBill = bill + tipAmount;
+let each = totalBill / people;
+
+document.getElementById("tipAmount").innerText = tipAmount.toFixed(2);
+document.getElementById("totalBill").innerText = totalBill.toFixed(2);
+document.getElementById("amountPerPerson").innerText = each.toFixed(2);
 }
-function printOutput(num) {
-    if (num == "") {
-        document.getElementById("output-value").innerText = num;
-    }
-    else {
-        document.getElementById("output-value").innerText = getFormattedNumber(num);
-    }
+
+function resetFields() {
+    document.getElementById("billAmount").value = "";
+    document.getElementById("numberOfPeople").value = "";
+    document.getElementById("tipPercentage").value = "";
+   
+    document.getElementById("tipAmount").innerText = "0.00";
+    document.getElementById("totalBill").innerText = "0.00";
+    document.getElementById("amountPerPerson").innerText = "0.00";
 }
-function getFormattedNumber(num) {
-    if (num == "-") {
-        return "";
-    }
-    var n = Number(num);
-    var value = n.toLocaleString("en");
-    return value;
-}
-function reverseNumberFormat(num) {
-    return Number(num.replace(/,/g, ''));
-}
-var operator = document.getElementsByClassName("operator");
-for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', function () {
-        if (this.id == "clear") {
-            printHistory("");
-            printOutput("");
-        }
-        else if (this.id == "backspace") {
-            var output = reverseNumberFormat(getOutput()).toString();
-            if (output) {//if output has a value
-                output = output.substr(0, output.length - 1);
-                printOutput(output);
-            }
-        }
-        else {
-            var output = getOutput();
-            var history = getHistory();
-            if (output == "" && history != "") {
-                if (isNaN(history[history.length - 1])) {
-                    history = history.substr(0, history.length - 1);
-                }
-            }
-            if (output != "" || history != "") {
-                output = output == "" ? output : reverseNumberFormat(output);
-                history = history + output;
-                if (this.id == "=") {
-                    var result = eval(history);
-                    printOutput(result);
-                    printHistory("");
-                }
-                else {
-                    history = history + this.id;
-                    printHistory(history);
-                    printOutput("");
-                }
-            }
-        }
-    });
-}
-var number = document.getElementsByClassName("number");
-for (var i = 0; i < number.length; i++) {
-    number[i].addEventListener('click', function () {
-        var output = reverseNumberFormat(getOutput());
-        if (output != NaN) { //if output is a number
-            output = output + this.id;
-            printOutput(output);
-        }
-    });
-}   
